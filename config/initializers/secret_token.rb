42ab78a2a44e592c9b9a7a255f5048c08dd306a7
Keturah Walters <your.ketwalters@gmail.com>
2014-03-14 14:22:05 -0400

    add rails assessment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsAssessment::Application.config.secret_token = 'fb96a3132e4745e9157de4a592e986af3e7546a72d48636abc53b8f2055314645abfa8b587f8f1fd1a335a1e0c79b78c3bb740e185f5339c97a9659e5bd12b9a'
